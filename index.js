912cf4c1df223708c561cc022bf5832cc3719458
“tanya-ananya” <taaruni.ananya@gmail.com>
2025-05-07 00:09:54 -0400

    dark mode updated

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/*** You will not need this file until Unit 5 ***/
+// Step 1: Select the theme button
+let themeToggle = document.querySelector(".switch input");
+
+// Step 2: Write the callback function
+const toggleDarkMode = () => {
+    document.body.classList.toggle("dark-mode");
+}
+themeToggle.addEventListener("change", toggleDarkMode);
+
+
+
+// Step 1: Add your query for the submit RSVP button here
+const addParticipant = (event) => {
+    // Step 2: Write your code to manipulate the DOM here
+    event.preventDefault();
+}
+
+// Step 3: Add a click event listener to the submit RSVP button here
+
+
+/*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
+/*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
+/*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
